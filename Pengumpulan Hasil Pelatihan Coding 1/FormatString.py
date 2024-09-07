7ae4364fb5e079f2589f8854b392c4ce7006361e
octavia136 <sitirosiatioctavia@gmail.com>
2024-09-07 12:33:08 +0700

    Add files via upload

---
+++ Pengumpulan Hasil Pelatihan Coding 1/FormatString.py
@@ -0,0 +1,9 @@
+nama = "octavia"
+pesan = f"nama saya (nama)"
+print(pesan)
+
+umur = 18 
+pesanTambahan = "umur saya adalah {}". format(umur)
+pesanLengkap = f"nama: {nama} dan umur : {umur}"
+print(pesanTambahan)
+print(pesanLengkap)
---
+++ Pengumpulan Hasil Pelatihan Coding 1/index.py
@@ -0,0 +1,7 @@
+nama = "octavia"
+umur = 18
+
+nama = "siti"
+print (nama)
+
+print (nama)
