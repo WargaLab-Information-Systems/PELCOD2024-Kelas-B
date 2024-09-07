079575ab069bdc1c77ac80fb1f61df12228e5656
Chelsy <chelsysinaga44@gmail.com>
2024-09-07 12:11:36 +0700

    Pelatihan Hari Pertama

---
+++ Pengumpulan Hasil Pelatihan Coding 1/146_Chelsy-Sinaga/FormatString.py
@@ -0,0 +1,12 @@
+nama = "Chelsy"
+pesan = f"Nama Saya {nama}"
+print(pesan)
+
+umur = 21
+pesanTambahan = "Umur saya adalah {}".format(umur)
+pesanLengkap = f"Nama : {nama} dan Umur : {umur}"
+print(pesanTambahan)
+print(pesanLengkap)
+
+pesanManual = "nama saya yang benar adalah" + nama + " dan Berumur " + str(umur)
+print(pesanManual)
