70519fa65c3795d510d40268413388f22eb01dda
Muhammad Naufaldi Arzaki <Muhammadnaufaldi160@gmail.com>
2024-09-07 12:28:46 +0700

    Add files via upload

---
+++ 054_Muhammad-Naufaldi-Arzaki/gerbanglogika.py
@@ -0,0 +1,8 @@
+a = 10
+b = 6
+
+perbandingan = not( (a > b) or (b > a) )
+# true and true
+
+print(perbandingan)
+
---
+++ 054_Muhammad-Naufaldi-Arzaki/operasiaritmatika.py
@@ -0,0 +1,25 @@
+import math
+
+a = 10
+b = 5
+
+# hasil = a + b
+# print(hasil)
+
+# hasil = a + b
+# print(hasil)
+
+# hasil = a + b
+# print(nama)
+
+# hasil = a / b
+
+
+# hasil = a  * (b + a)
+# print(hasil)
+
+hasil = b * b * b
+print(hasil)
+
+akar = math.sqrt(hasil + a)
+print(akar)
