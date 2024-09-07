2701eb4915d8034a1d8be4c3b105ddfd361eadbc
kinanti <Dwikinan24@gmail.com>
2024-09-07 12:11:25 +0700

    pelatihan hari pertama

---
+++ Pengumpulan Hasil Pelatihan Coding 1/022_Dyah-fajar-dwi-kinanti/belajar python/FormatString.py
@@ -0,0 +1,13 @@
+nama = "dyah"
+pesan = f"Nama Saya {nama}"
+print(pesan)
+
+umur = 18
+pesanTambahan ="Umur Saya adalah {}".format (umur)
+pesanLengkap = f"Nama : {nama} dan Umur : {umur}"
+print(pesanTambahan)
+print(pesanLengkap)
+
+pesanManual = "nama saya yang benar adalah " + nama + " dan berumur " + str(umur)
+print(pesanManual)
+
