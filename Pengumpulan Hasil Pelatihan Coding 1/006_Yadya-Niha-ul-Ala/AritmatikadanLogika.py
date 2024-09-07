e2381d3af395a6668a5f05ebea588324308917de
Niha <yadyanihaulala@gmail.com>
2024-09-07 12:11:09 +0700

    Pelatihan Hari pertama

---
+++ Pengumpulan Hasil Pelatihan Coding 1/006_Yadya-Niha-ul-Ala/AritmatikadanLogika.py
@@ -0,0 +1,27 @@
+import math 
+
+a = 10
+b = 5
+
+# hasil = a + b
+# print(f"Hasil Pertambahan: {hasil}")
+
+# hasil = a - b
+# print(f"Hasil Pengurangan: {hasil}")
+
+# hasil = a *b
+# print(f"Hasil Perkalian: {hasil}")
+
+# hasil = a / b
+# print(f"Hasil Pembagian: {hasil}")
+
+# hasil = a * (b + a)
+# print(f"Hasil: {hasil}")
+
+#** = untuk pangkat, misal pangkat 3 berarti **3
+hasil = b**3
+print(hasil)
+
+#math di sini untuk memanggil library yg diimport sebelumnya
+akar = math.sqrt(hasil + a)
+print(akar)
