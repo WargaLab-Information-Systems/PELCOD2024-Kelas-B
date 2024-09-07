1169906ded5dfed49b5e12ef891ad39e78905997
srimanik <srilamina25@gmail.com>
2024-09-07 12:12:59 +0700

    Pelatihan Hari Pertama

---
+++ Pengumpulan Hasil Pelatihan Coding 1/138_Sri-Lamina Manik/FormatString.py
@@ -0,0 +1,12 @@
+nama = "sri"
+pesan = f"Nama Saya {nama}"
+print(pesan)
+
+umur = 18
+pesanTambahan = "Umur saya adalah {}".format(umur)
+pesanLengkap =f"Nama : {nama} dan Umur : (umur)"
+print(pesanTambahan)
+print(pesanTambahan)
+
+pesanManual = "nama saya yang benar adalah " + nama + " dan Berumur " + str(umur)
+print(pesanManual)
---
+++ Pengumpulan Hasil Pelatihan Coding 1/138_Sri-Lamina Manik/variabel.py
@@ -0,0 +1,7 @@
+nama = "sri"
+umur = 18
+
+print (nama)
+
+nama = "sri"
+print (nama)
