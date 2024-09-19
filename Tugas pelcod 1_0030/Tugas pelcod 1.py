f034e32ead4ec0a7fd2a5238f4a5c8d9af8b7d07
BimaSocaPangestu <bimasocapanges7@gmail.com>
2024-09-19 17:29:26 +0700

    240441100030

---
+++ Tugas pelcod 1_0030/Tugas pelcod 1.py
@@ -0,0 +1,25 @@
+print("Pada suatu hari di sebuah toko buah terdapat dua orang anak yang ingin membeli buah")
+anak1 = input("berikan nama untuk anak pertama: ")
+anak2 = input("berikan nama untuk anak kedua: ")
+print(f"{anak2} : Kamu mau beli buah apa {anak2}?")
+print(f"{anak2} : Aku mau beli buah melonnl")
+print(f"{anak1} : Ohhh kalo aku mau beli buah semangka")
+print(f"{anak2} : Pak ini buahnya berapa ya pak?")
+print("Pak penjual : Untuk buah melon harganya Rp 5000 kalo semangka Rp 8000")
+print(f"{anak2} : Pak aku sama temen ku beli buah semangka sama melonnya")
+print("Pak penjual : Bual melonn nya berapa, buah semangkanya berapa?")
+melon = int(input("Masukkan jumlah buah melon: "))
+semangka = int(input("Masukkan jumlah buah semangka: "))
+
+# Operasi aritmatika
+hargamelon = melon * 5000
+hargasemangka = semangka * 7000
+
+print(f"Pak penjual : Total harganya {hargamelon + hargasemangka}")
+print(f"{anak2} : Iya pak ini uangnya")
+print("Pak penjual : Iya dek terimakasih banyak")
+
+# Operasi logika
+print(f"apakah jumlah semangka lebih banyak dari melon? : {semangka > apel}")
+
+print("Akhirnya mereka pun pulang dan makan buah bersama")
