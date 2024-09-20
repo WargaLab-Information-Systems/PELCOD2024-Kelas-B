c1d3e1b9e3cf7f260d82eb12f26cc769ee600daf
azhara <azahrafera96@gmail.com>
2024-09-20 15:18:08 +0700

    240441100050

---
+++ Pengumpulan Tugas/240441100050_AZHARA VERA HARIANI/Tugaspelcod1.py
@@ -0,0 +1,29 @@
+#percakapan antara teman
+print("pada suatu hari, Vera dan Vira sedang ada di taman")
+
+#input karakter
+nama_1 = ("Vera")
+nama_2 = ("Vira")
+
+print("hai,perkenalkan nama aku Vera")
+print("iya salam kenal namaku Vira")
+print(f"{nama_2}: tahun ini kamu berumur berapa?")
+umur_Vera = int(input(f"{nama_1}:umurmu sekarang berapa"))
+print(f"{nama_2}: oh, jadi umurmu {umur_Vera} tahun. umurku sekarang {umur_Vera + 3}")
+print(f"{nama_1}: oh ternyata selisih umur kita {3} tahun ya.")
+selisih_umur = abs (umur_Vera - (umur_Vera + 3))
+print(f"{nama_2}: ya benar selisih umur kita adalah {selisih_umur} tahun.")
+print(f"{nama_1}: bagaimana kalau kita menghitung umur kita?")
+total_umur = umur_Vera + (umur_Vera + 3)
+print(f"{nama_2}: total umur kita adalah {total_umur} tahun.")
+
+print(f"{nama_1}: ngomong-ngomong,apa cita-citamu vira?")
+print(f"{nama_2}: cita-citaku menjadi seorang dokter. Bagaimana denganmu?")
+print(f"{nama_1}: wah, ternyata kita punya cita-cita yang sama, yaitu menjadi seorang dokter!")
+print(f"{nama_2}: benar sekali, Vera. Ayo kita kejar dan mengusahakan impian kita")
+print(f"{nama_1}: yuk kita harus belajar,dan berdoa dengan sungguh-sungguh")
+print(f"{nama_2}: dan kita tidak boleh mengecewakan orang tua kita")
+print(f"{nama_1}: benar sekali, sampai jumpa hati-hati dijalan")
+
+#Akhir cerita
+print(f"{nama_1} dan {nama_2} pergi dan saling mengejar cita-cita mereka")
